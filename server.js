acc4462d9d27b629bccc13ffbc96db09f925ac15
Joa0Zera <joao.v.oliveira139@aluno.senai.br>
2024-09-16 09:35:25 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+// Importar o módulo do pacote 
+var readline = require('readline-sync')
+
+// Consumir a Variavel
+var nome= readline.question("Digite o seu nome? \n")
+var idade= readline.questionInt("Digite a sua idade? \n")
+
+
+console.log("O nome digitado foi " + nome + "e sua idade é: " + idade + "anos") // Imprimir no Terminal digitando "node (server.js)"
+
+
+
+
+
+
+
+
+
+// Para limpar o terminal --> Clear
+
+
+
+
+
